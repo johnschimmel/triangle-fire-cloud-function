7f07be95d2453efaa4eddd68a3dbba45f862a942
John Schimmel <john.schimmel@gmail.com>
2019-11-17 19:55:29 -0500

    adding in cors

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var api = require('./api');
 
 var cachedData = null;
 
-exports.triangleData = (req, res) => {
+var handleCors = (res) => {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+}
+
+var handleData = (res) => {
     if (cachedData != null) {
         res.json(cachedData);
     } else {
@@ -14,10 +22,20 @@ exports.triangleData = (req, res) => {
             } else {
                 cachedData = data;
                 res.json(data)
-            }
-            
-        })
-        
+            }  
+        })     
+    }
+}
+
+exports.triangleData = (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
+    if (req.method === 'OPTIONS') {
+        handleCors(res);
+    } else {
+        handleData(res);
     }
     
+    
 }
+
